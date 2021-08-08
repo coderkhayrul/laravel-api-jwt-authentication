300143bfc18217ea109b352adfc35ba191023698
Coderkhayrul <coderkhayrul>
2021-08-08 16:09:29 +0600

    Table Migration

---
+++ database/migrations/2021_08_08_100408_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            // $table->integer('user_id')->unsigned();
+            $table->unsignedInteger('user_id');
+            $table->string('title', 80);
+            $table->text('description')->nullable();
+            $table->integer('total_videos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
