e3263b99da619ba2a7c2002a83bd6b342517c3d5
Coderkhayrul <coderkhayrul>
2021-08-09 20:52:37 +0600

    Create API route and middlewar

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::group(['middleware' => ['api']], function () {
+    Route::get('/profile', [UserController::class, 'profile']);
+    Route::get('/logout', [UserController::class, 'logout']);
+
+    // COUESES API ROUTES
+    Route::post('/course-enrol', [CourseController::class, 'courseEnrollment']);
+    Route::get('/total-course', [CourseController::class, 'totalcourses']);
+    Route::get('/delete-course/{id}', [CourseController::class, 'deleteCourse']);
+});
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
