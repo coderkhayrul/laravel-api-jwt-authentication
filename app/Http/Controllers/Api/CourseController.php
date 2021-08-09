8693bbec71da12976b61dab328e11ebee049e797
Coderkhayrul <coderkhayrul>
2021-08-09 22:44:52 +0600

    User Couese Enrolment Api Work

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -5,12 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CourseController extends Controller
 {
     // COURSE ENROLLMENT API - POST
     public function courseEnrollment(Request $request)
     {
+        // Validation
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'total_videos' => 'required',
+        ]);
+
+        // Create Course Object
+        $course = new Course();
+        $course->user_id = Auth::user()->id;
+        $course->title = $request->title;
+        $course->description = $request->description;
+        $course->total_videos = $request->total_videos;
+
+        $course->save();
+
+        // send Response
+        return response()->json([
+            'status' => 1,
+            'message' => 'Courses Enrollment Successfully'
+        ], 200);
     }
 
     // COURSE TOTALCOURSES API - GET
