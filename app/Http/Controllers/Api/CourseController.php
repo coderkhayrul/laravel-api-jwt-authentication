da895e26d31feca7bf8f42accc7244b1bc0650d1
Coderkhayrul <coderkhayrul>
2021-08-09 20:43:32 +0600

    Api controller Setting

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // COURSE ENROLLMENT API - POST
+    public function courseEnrollment(Request $request)
+    {
+    }
+
+    // COURSE TOTALCOURSES API - GET
+    public function totalcourses()
+    {
+    }
+
+    // DELETE COURSE API - GET
+    public function deleteCourse($id)
+    {
+    }
+}
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // USER REGISTER  API - POST
+    public function register(Request $request)
+    {
+    }
+    // USER LOGIN API - POST
+    public function login(Request $request)
+    {
+    }
+    // USER PROFILE API - GET
+    public function profile($id)
+    {
+    }
+    // USER LOGOUT API - GET
+    public function logout()
+    {
+    }
+}
